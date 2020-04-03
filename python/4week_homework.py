ae9f7c8e39de34877aa117c20ad4725a4c43eb27
MalMyeong <61974115+MalMyeong@users.noreply.github.com>
2020-04-04 03:51:13 +0900

    Update 4week_homework.py
    index error롤 get, post요청에 대해 반응이 없음
    select box의 자료 request가 맞는지 확인 필요

--- python/4week_homework.py
+++ python/4week_homework.py
@@ -9,5 +9,25 @@ db = client.dbsparta                      # 'dbsparta'라는 이름의 db를 만
 def home():
     return render_template('4week.html')
 
+@app.route('/order', methods=['POST'])
+def write_order():
+    name = request.form['name']
+    address = request.form['address']
+    phone = request.form['phone']
+    color = request.form['color']
+    doc = {
+        'name': name,
+        'color': color,
+        'address': address,
+        'phone': phone
+    }
+    db.order.insert_one(doc)
+    return jsonify({'result': 'success', 'msg': '주문이 성공적으로 접수되었습니다.'} )
+
+@app.route('/order', methods=['GET'])
+def read_order():
+    order = list(db.order.find({},{'_id':0}))
+    return jsonify({'result': 'success', 'order': order})
+
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('localhost', port=5000, debug=True)
